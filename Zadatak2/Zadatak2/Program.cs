eaf243d9e934ab72815a1a6ea4e17732e150d534
Nix223 <nikola.kudoic1@gmail.com>
2017-10-22 19:16:39 +0200

    first commit

--- Zadatak2/Zadatak2/Program.cs
+++ Zadatak2/Zadatak2/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,108 @@ namespace Zadatak2
 {
     class Program
     {
-        static void Main(string[] args)
+        public class GenericList<X> : IGenericList<X>
         {
+            private X[] _internalStorage;
+            private int pozicija = 0;
+
+            public GenericList()
+            {
+                _internalStorage = new X[4];
+            }
+
+            public GenericList(int InitialSize)
+            {
+                _internalStorage = new X[InitialSize];
+            }
+
+
+            public void Add<X>(X item)
+            {
+                if (pozicija >= _internalStorage.Length)
+                {
+                    Array.Resize(ref _internalStorage, _internalStorage.Length * 2);
+                }
+                _internalStorage[pozicija] = item;
+                pozicija++;
+            }
+
+            public bool RemoveAt(int index)
+            {
+                if (index > _internalStorage.Length)
+                {
+                    throw new IndexOutOfRangeException("Given index is out of bounds.");
+                }
+                for (int i = index; i < _internalStorage.Length; i++)
+                {
+                    _internalStorage[i] = _internalStorage[i + 1];
+                }
+                pozicija--;
+                return true;
+            }
+
+            public bool Remove<X>(X item)
+            {
+                for (int i = 0; i < _internalStorage.Length; i++)
+                {
+                    if (_internalStorage[i].Equals(item))
+                    {
+                        RemoveAt(i);
+                    }
+                }
+                return false;
+            }
+
+            public X GetElement<X>(int index)
+            {
+                if (index <= pozicija)
+                {
+                    return _internalStorage[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Given index is out of bounds");
+                }
+            }
+
+            public int IndexOf<X>(X item)
+            {
+                for (int i = 0; i < _internalStorage.Length; i++)
+                {
+                    if (_internalStorage[i].Equals(item))
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+
+            public int Count
+            {
+                get { return pozicija + 1; }
+            }
+
+            public void Clear()
+            {
+                pozicija = 0;
+            }
+
+            public bool Contains<X>(X item)
+            {
+                for (int i = 0; i < _internalStorage.Length; i++)
+                {
+                    if (_internalStorage[i].Equals(item))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+
+
+
+
         }
     }
 }
